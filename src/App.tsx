306cdc329d6b4542958f6f2396201f26b1229f47
Marc Scherrer <marc.scherrer@hsr.ch>
2023-02-14 21:37:20 +0100

    Add some thingies

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Images from "./Images";
+import { Alert, ButtonGroup, Button } from "react-bootstrap";
 
 function App() {
-  const [showImage, setShowImage] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  const [isBroken, setIsBroken] = useState<boolean>(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setMessage((e.target as HTMLInputElement).value);
+  };
+
   return (
-    <div className="container">
-      <h1>H1</h1>
-      <h2>H2</h2>
-      <h3>H3</h3>
-      <h4>H4</h4>
-      <h5>H5</h5>
-      <h6>H6</h6>
-      <pre>Pre</pre>
-      <code>Code</code>
-      <pre>
-        <code>Code in pre</code>
-      </pre>
-      <p className="lead">Open an image by clickling</p>
-      <div className="btn-group btn-group-vertical">
-        <button
-          className="btn btn-primary mb-2"
-          type="button"
-          onClick={() => setShowImage(!showImage)}
-        >
-          Open image
-        </button>
-      </div>
-      {showImage && <Images />}
-      <div className="jumbotron">this is riveting</div>
-    </div>
+    <>
+      {isBroken && (
+        <Alert variant="danger">
+          <h1>Uh oh, you broke the website!</h1>
+          <p>But don't worry. It's going to be okay.</p>
+        </Alert>
+      )}
+      {!isBroken && (
+        <div className="container pt-4">
+          <Alert variant="success">Yayy, I did it!</Alert>
+          <ButtonGroup>
+            <Button
+              variant="outline-success"
+              value="I love you"
+              onClick={handleClick}
+            >
+              Meli
+            </Button>
+            <Button
+              variant="outline-warning"
+              onClick={handleClick}
+              value="WTF are you doing here?"
+            >
+              Someone else
+            </Button>
+            <Button
+              variant="outline-danger"
+              onClick={() => setIsBroken(true)}
+              value="Uh oh, you broke the website :("
+            >
+              Mäge
+            </Button>
+          </ButtonGroup>
+
+          {message && <p>{message}</p>}
+        </div>
+      )}
+    </>
   );
 }
 
